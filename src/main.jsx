ee25ed8509c247ffbe8631221bec62b4bea406f8
Fouche du Preez <fouche@limehouse.co.za>
2024-05-21 21:24:43 +0200

    added state component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppClass from "./AppClass.jsx";
 import HelloWorld from "./HelloWorld.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -8,7 +7,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <div className="container">
           <div className="row">
               <div className="col">
-                  <AppClass msg="Hello, world!" />
                   <HelloWorld msg="Hello, again!"/>
               </div>
           </div>
